ac5a7d7ed97a1550a945dd94e587d8173f399129
Charan Kumar Krishnaiah <charankumark.1rn12cs0222gmail.com>
2020-08-08 19:23:39 +0530

    comments removed

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,8 +1,8 @@
 export const FireBaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
+  apiKey: "AIzaSyCiWOWqn3j7DDlO9ml4UO2-vGl4vXYKk78",
+  authDomain: "food-on-wheels-bb063.firebaseapp.com",
+  databaseURL: "https://food-on-wheels-bb063.firebaseio.com",
+  projectId: "food-on-wheels-bb063",
+  storageBucket: "food-on-wheels-bb063.appspot.com",
+  messagingSenderId: "672441046573",
 };
